3b2a8ab0677e852abec9828b0589afb37317069d
Gregory Varias <gregvarias@gmail.com>
2013-10-13 01:46:52 -0700

    initial commit. set up postgres and rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjGlb::Application.config.secret_token = '6b7e8239fdb1d43bbe499da66abbcaffcba5de948a72742f1f607f5796a51e4685f471cf4dc3182d09bd2d50b1e3109b5a5648572b7c8f2cdb16311035435414'
