971a017508f108454127dfe098076acdf71730ec
Gregory Varias <gregvarias@gmail.com>
2013-10-14 20:18:26 -0700

    deleted unused code in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,33 +2,21 @@ class SessionsController < ApplicationController
 
   def new
   end
-  
-  # def create
-  #   @user = User.find_or_create_from_auth_hash(auth_hash)
-  #   puts '*' * 80
-  #   ap auth_hash
-  #   # self.current_user = @user
-  #   redirect_to '/user/index'
-  # end
 
-def create
-  authorization = Authorization.find_or_create_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
-  puts '*' * 80
-  ap authorization
+  def create
+    authorization = Authorization.find_or_create_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+    puts '*' * 80
+    ap authorization
 
-  if authorization
-    user = User.find_by_name(authorization.user.name)
-  else
-    user = User.new :name => auth_hash[:info][:nickname]
-    user.authorizations.build :provider => auth_hash[:provider], :uid => auth_hash[:uid]
-    user.save 
-  end
-  
-  redirect_to user_path(user)
-end
-
-
-  def failure
+    if authorization
+      user = User.find_by_name(authorization.user.name)
+    else
+      user = User.new :name => auth_hash[:info][:nickname]
+      user.authorizations.build :provider => auth_hash[:provider], :uid => auth_hash[:uid]
+      user.save 
+    end
+    
+    redirect_to user_path(user)
   end
 
   protected
