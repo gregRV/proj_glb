bae1781fddeb3a7f5e9edae4d2e4936d750ba9e5
Gregory Varias <gregvarias@gmail.com>
2013-10-24 16:24:52 -0700

    added session helper methods to check for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def logged_in?
+		session[:user_id]
+	end
+
+	def current_user
+		user = User.find(session[:user_id])
+	end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,23 @@
 class SessionsController < ApplicationController
 
   def new
+    if logged_in?
+      ap "ALREADY LOGGED IN"
+      redirect_to user_path(current_user)
+    end
+    ap "NOT LOGGED IN"
   end
 
   def create
     authorization = Authorization.find_or_create_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
     if authorization
       user = User.find_by_username(authorization.user.username)
+      session[:user_id] = user.id
     else
       user = User.new :username => auth_hash[:info][:nickname]
       user.authorizations.build :provider => auth_hash[:provider], :uid => auth_hash[:uid], :token => auth_hash.credentials.token
       user.save
+      session[:user_id] = user.id
       client = User.create_client(user)
       User.update_user_network(client, user)
     end
